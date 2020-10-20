4dbd70efd9c5ac4e2f8dd82648563f2081bf65cc
GrayBreath <sinchik97@mail.ru>
2020-10-20 17:28:08 +0300

    Gameplay Implem

--- Assets/CameraAttach.cs
+++ Assets/CameraAttach.cs
@@ -7,8 +7,17 @@ public class CameraAttach : MonoBehaviour
 {
 
     public Animator anim,gun_anim;
-    public GameObject cam,bullet;
-    
+    public GameObject cam, bullet, camera_place,bulettTrace;
+
+
+    public bool trigger;
+
+    public Vector3 start_pos, end_pos;
+    public Quaternion start_angle, end_angle;
+
+    public float time_slider;
+
+
     public void StartGame() 
     {
 
@@ -21,6 +30,16 @@ public class CameraAttach : MonoBehaviour
         cam.transform.parent = bullet.transform;
         bullet.transform.parent = null;
         bullet.GetComponent<BulletController>().enabled = true;
+       // if (gameObject.name == "Camera") { 
+            start_pos = cam.transform.localPosition;
+            end_pos = camera_place.transform.localPosition;
+        start_angle = cam.transform.rotation;
+        end_angle = camera_place.transform.rotation;
+
+
+        bulettTrace.SetActive(true);
+        trigger = true;
+        //}
     }
     public void ActivateAnimatorOnGun() 
     {
@@ -36,12 +55,26 @@ public class CameraAttach : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
+    void FixedUpdate() 
+    {
+        if (trigger)
+        {
+            time_slider += 0.02f;
+        }
+    }
     // Update is called once per frame
     void Update()
-    {
-        
+    {  
+        if (trigger) 
+        {
+            cam.transform.localPosition = Vector3.Lerp(start_pos, end_pos,time_slider);
+
+            cam.transform.rotation = Quaternion.Lerp(start_angle, end_angle, time_slider);
+
+        }
+
     }
 }
--- Assets/DestroyTarget.cs
+++ Assets/DestroyTarget.cs
@@ -8,6 +8,9 @@ public class DestroyTarget : MonoBehaviour
     public int points;
     public GameManager gm;
     public GameObject textPoints;
+
+    public SFXManager s_manager;
+    public GameObject bul;
     public void InstantRubish()
     {
       //TODO
@@ -29,14 +32,41 @@ public class DestroyTarget : MonoBehaviour
         coins_text.GetComponent<Rigidbody>().velocity  = new Vector3(0,5,10);        
         Destroy(coins_text,2f);
         Destroy(gameObject);
+         
+            var my_tag = gameObject.tag;
+           
+            switch (my_tag)
+            {
+                case "tree":
 
-      }
+                    s_manager.source.PlayOneShot(s_manager.woods[Random.Range(0, s_manager.woods.Length)]);
+                    Destroy(Instantiate(s_manager.dust,bul.transform.position,bul.transform.rotation),2f);
+                    break;
+                case "burrel":
+
+                  
+                    s_manager.source.PlayOneShot(s_manager.explo);
+                    Destroy(Instantiate(s_manager.explosion, bul.transform.position, bul.transform.rotation), 2f);
+
+
+                    //Console.WriteLine("Case 2");
+                    break;
+
+                default:
+                    //Console.WriteLine("Default case");
+                    break;
+            }
+
+
+        }
 
     }
     // Start is called before the first frame update
     void Start()
     {
       gm  = GameObject.Find("GameManager").GetComponent<GameManager>();
+      s_manager = GameObject.Find("Camera").GetComponent<SFXManager>();
+        bul = GameObject.Find("Bullet");
     }
 
     // Update is called once per frame
